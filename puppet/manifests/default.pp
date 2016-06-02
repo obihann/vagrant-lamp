a6aecb1ebeb5210fcfeaf1a7bdf595c4ace0a11a
Jeff Hann <jeff.hann@redspace.com>
2016-06-02 10:50:21 -0300

    adding redis, memcached, sqlite, and composer

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,9 +1,18 @@
 # Configure apt to update for installation of every package
 Class['apt::update'] -> Package <| provider == 'apt' |>
 
+package { [
+  "redis",
+  "memcached",
+  "sqlite"
+  ]:
+  ensure => "installed"
+}
+
 # Include our modules
 include apt
 include base
 include php
 include apache::php
 include mysql
+include composer
